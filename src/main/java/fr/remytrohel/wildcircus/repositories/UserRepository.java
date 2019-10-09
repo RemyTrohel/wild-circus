df0555f50e710c1b0d48dc9a842b339b54d70cb0
Rémy Trohel <remy.trohel@gmail.com>
2019-10-09 15:35:50 +0200

    added annotation

--- src/main/java/fr/remytrohel/wildcircus/repositories/UserRepository.java
+++ src/main/java/fr/remytrohel/wildcircus/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package fr.remytrohel.wildcircus.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import fr.remytrohel.wildcircus.entity.User;
+import fr.remytrohel.wildcircus.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    public User findByMail(String mail);
 }
