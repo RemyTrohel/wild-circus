0481ddcae687c6bf16c9c59914be393e27b4d6e2
Rémy Trohel <remy.trohel@gmail.com>
2019-10-10 16:50:52 +0200

    added constructors and getter/setter

--- src/main/java/fr/remytrohel/wildcircus/entities/Category.java
+++ src/main/java/fr/remytrohel/wildcircus/entities/Category.java
@@ -21,4 +21,42 @@ public class Category {
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private Set<Ticket> tickets = new TreeSet<Ticket>();
+
+    public Category(Long id, String name, Set<Ticket> tickets) {
+        this.id = id;
+        this.name = name;
+        this.tickets = tickets;
+    }
+
+    public Category() {
+    }
+
+    public Category(String name, Set<Ticket> tickets) {
+        this.name = name;
+        this.tickets = tickets;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(Set<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
