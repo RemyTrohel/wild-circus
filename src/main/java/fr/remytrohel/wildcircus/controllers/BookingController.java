434ea627849121978943e264c1c4e05535d0eaa1
Rémy Trohel <remy.trohel@gmail.com>
2019-10-10 18:31:49 +0200

    added ticket formulaire, wip

--- src/main/java/fr/remytrohel/wildcircus/controllers/BookingController.java
+++ src/main/java/fr/remytrohel/wildcircus/controllers/BookingController.java
@@ -2,7 +2,9 @@ package fr.remytrohel.wildcircus.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,8 +23,15 @@ public class BookingController {
     }
 
     @GetMapping("/bookings/{id}/edit")
-    public String addTicket() {
+    public String addTicket(Model model, @PathVariable Long id) {
+        Booking booking = bookingRepository.findById(id).get();
+        model.addAttribute("booking", booking);
         return "ticket";
+    }
+
+    @GetMapping("/bookings/{id}")
+    public String shoppingCart() {
+        return "cart";
     }
 
     @PostMapping("/bookings")
