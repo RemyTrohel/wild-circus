ae075f136f0cddab68c02793c6994586e1d8ad4a
Rémy Trohel <remy.trohel@gmail.com>
2019-10-10 16:48:22 +0200

    added constructors and getter/setter

--- src/main/java/fr/remytrohel/wildcircus/entities/Ticket.java
+++ src/main/java/fr/remytrohel/wildcircus/entities/Ticket.java
@@ -1,8 +1,5 @@
 package fr.remytrohel.wildcircus.entities;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,4 +20,52 @@ public class Ticket {
 
     @ManyToOne
     private Category category;
+
+    public Ticket(Long id, int quantity, Booking booking, Category category) {
+        this.id = id;
+        this.quantity = quantity;
+        this.booking = booking;
+        this.category = category;
+    }
+
+    public Ticket(int quantity, Booking booking, Category category) {
+        this.quantity = quantity;
+        this.booking = booking;
+        this.category = category;
+    }
+
+    public Ticket() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
